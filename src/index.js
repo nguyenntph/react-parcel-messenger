8c1e40fd65b447b5a518a71e53c7935f73af0f9e
nguyenntph <nguyen.ntph@gmail.com>
2020-05-18 07:58:31 +0700

    Init parcel react

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from './components/Header'
+import './styles/index.scss'
+
+class HelloMessage extends React.Component {
+    render() {
+        return <div>
+            <Header/>
+            <div className="container">
+                <h1>Hello {this.props.name}</h1>
+            </div>
+        </div>
+    }
+}
+
+let App = document.getElementById("app");
+
+ReactDOM.render(<HelloMessage name="Yomi" />, App);
