f73895e5064b63a3a3cbe98f4f29994a90dc15f3
nguyenntph <nguyen.ntph@gmail.com>
2020-05-22 05:48:25 +0700

    Submit chat message

--- src/components/ChatBox/Form.js
+++ src/components/ChatBox/Form.js
@@ -1,35 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Form = ({ submit }) => (
-  <form
-    onSubmit={submit}
-    className="flex h-20 absolute inset-x-0 px-1 justify-between"
-    style={{ bottom: 40 }}
-  >
-    <div>
-      <button className="w-8 h-8 mx-1 text-blue-500">
-        <FontAwesomeIcon icon="plus" />
+const Form = ({ submit }) => {
+  const [content, setContent] = useState("");
+  const inputChangeHandler = event => setContent(event.target.value);
+  return (
+    <form
+      onSubmit={event => {
+        event.preventDefault();
+        submit(content);
+        setContent("");
+      }}
+      className="flex h-20 absolute inset-x-0 px-1 justify-between"
+      style={{ bottom: 40 }}
+    >
+      <div>
+        <button className="w-8 h-8 mx-1 text-blue-500">
+          <FontAwesomeIcon icon="plus" />
+        </button>
+        <button className="w-8 h-8 mx-1 text-blue-500">
+          <FontAwesomeIcon icon="edit" />
+        </button>
+        <button className="w-8 h-8 mx-1 text-blue-500">
+          <FontAwesomeIcon icon="file" />
+        </button>
+        <button className="w-8 h-8 mx-1 text-blue-500">
+          <FontAwesomeIcon icon="video" />
+        </button>
+      </div>
+      <input
+        type="text"
+        name="content"
+        value={content}
+        onChange={inputChangeHandler}
+        className="bg-gray-200 rounded-full h-8 p-3 flex-grow outline-none"
+        placeholder="Type a message ..."
+      />
+      <button className="w-8 h-8 mx-1 text-blue-500 text-xl" type="submit">
+        <FontAwesomeIcon icon="paper-plane" />
       </button>
-      <button className="w-8 h-8 mx-1 text-blue-500">
-        <FontAwesomeIcon icon="edit" />
-      </button>
-      <button className="w-8 h-8 mx-1 text-blue-500">
-        <FontAwesomeIcon icon="file" />
-      </button>
-      <button className="w-8 h-8 mx-1 text-blue-500">
-        <FontAwesomeIcon icon="video" />
-      </button>
-    </div>
-    <input
-      type="text"
-      className="bg-gray-200 rounded-full h-8 p-3 flex-grow"
-      placeholder="Type a message ..."
-    />
-    <button className="w-8 h-8 mx-1 text-blue-500 text-xl" type="submit">
-      <FontAwesomeIcon icon="paper-plane" />
-    </button>
-  </form>
-);
+    </form>
+  );
+};
 
 export default Form;
